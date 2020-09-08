e8052c77848301ade90e9a92b069b53ad6e98c98
Brenson <brensonw300@gmail.com>
2020-09-08 11:38:56 -0700

    Changed code in a couple spots because just realized it didn't do what it was supposed to

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -1,21 +1,19 @@
 /*
 PROGRAMMERS Instructions
-
 Watch this short video:
 https://tk-assets.lambdaschool.com/1ea6e6a2-2ef1-45de-bcab-b99a8f775870_programmers.gif
-
 This component keeps track of a list of pioneers in the field of programming on the one hand,
 and the id of the currently featured programmer on the other. That's two slices of state!
 We can only feature one awesome programmer at a time.
-
 Find comments below to help you along.
 */
+
 
 import React, {useState} from 'react';
 
 // Use this variable ONLY to initialize a slice of state!
 // There is something in the JSX right now breaking this rule...
-export const listOfAwesome = [
+const listOfAwesome = [
   { id: '1', name: 'Ada Lovelace' },
   { id: '2', name: 'Grace Hopper' },
   { id: '3', name: 'Evelyn Boyd Granville' },
@@ -23,60 +21,73 @@ export const listOfAwesome = [
   { id: '5', name: 'Frances Allen' },
   { id: '6', name: 'Carol Shaw' },
 ];
-
 export default function Programmers() {
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
-const [programList, setProgramList] = useState(listOfAwesome);
-const [displayProgrammer, setDisplayProgrammer] = useState("");
+  // const [programList, setProgramList] = useState(listOfAwesome);
+  // const [displayProgammer, setDisplayProgramer] = useState("");
 
+  const [programmers, setProgrammers] = useState(listOfAwesome);
 
-  const getNameOfFeatured = (id) => {
+  console.log("programmers", programmers);
+  const [programmerId, setProgrammerId] = useState(null);
+  
+  const getNameOfFeatured = () => {
     // Leave this for last!
     // This is NOT an event handler but a helper function. See its usage inside the JSX.
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
-    programList.map(item => {
-      if(item.id === id) {
-        setDisplayProgrammer(item.name)
-        return item.name
-      }
+  //   programList.map(item => {
+  //     if(item.id === id) {
+  //       setDisplayProgramer(item.name)
+  //       return item.name
+  //     }
+  //   })
+  // };
+
+  if(programmerId === null) {
+    return null
+  } else {
+    const featuredProgrammer = programmers.find(programmer => {
+      return programmerId === programmer.id
     })
+    return featuredProgrammer.name
+  };
   };
 
   const style = {
     fontSize: '1.5em',
     marginTop: '0.5em',
-    color: 'royalblue', // 🤔 color turns to gold, when celebrating
+    color:programmerId !== null ? 'gold' :   'royalblue', // 🤔
   };
-
   return (
     <div className='widget-programmers container'>
       <h2>Programmers</h2>
       <div className='programmers'>
         {
+
           /* Nasty bug! We should map over a slice of state, instead of 'listOfAwesome'.
           We might think: "it works, though!" But if the list of programmers is not state,
           we could never add or edit programmers in the future. The list would be a static thing." */
-          programList.map(dev =>
-            <div className='programmer' key={dev.id}>
-              {dev.name} <button onClick={() => getNameOfFeatured(dev.id)}>Feature</button>
+          
+          programmers.map(dev =>
+            <div key={dev.id}>
+             
+              {dev.name} <button onClick={() => { setProgrammerId(dev.id) }}>Feature</button>
             </div>
           )
         }
       </div>
-      
-      
-        {
-          // Ternaries are fantastic to render "one thing or the other" depending on the "truthiness" of something.
-          // Pseudo-code: if the currently featured id is truthy render text 1, otherwise render text 2.
-          // Replace the hard-coded false with the correct variable.
-          displayProgrammer
-            ?<div style={style}> `🎉 Let's celebrate {displayProgrammer}! 🥳`</div>
-            : <div style={style}>'Pick an awesome programmer'</div>
-        }
-      </div>
- 
+      {
+        // Ternaries are fantastic to render "one thing or the other" depending on the "truthiness" of something.
+        // Pseudo-code: if the currently featured id is truthy render div 1, otherwise render div 2.
+        // Replace the hard-coded false with the correct variable.
+        
+        programmerId !== null
+          ? <div style={style}>🎉 Let&apos;s celebrate {getNameOfFeatured()}! 🥳</div>
+          : <div style={style}>Pick an awesome programmer</div>
+      }
+    </div>
   );
 }
--- src/components/Squares.js
+++ src/components/Squares.js
@@ -24,23 +24,39 @@ export default function Squares() {
   // 'activeSquare'. One holds the _array_ of square ids, and the other keeps track
   // of the currently active square. On page load there's no active square,
   // so the value of 'activeSquare' should be null.
-const [squares, setSquares] = useState(listOfSquareIds);
-const [activeSquare, setavtiveSquare] = useState("");
-  const isActive = id => {
+const [squares, setSquares] = useState(listOfSquareIds)
+const [activeSquare, setActiveSquare] = useState(null)
+  
+const getClassName = element => {
+  if (element === activeSquare) {
+    return 'active'
+  } else { 
+      return ''
+  }
+};
     // This is NOT a click handler but a helper, used inside the JSX (see below).
     // It should return a string containing the class name of 'active', if the id passed
     // as the argument matches the active square in state, empty string otherwise.
     // Right-click and "inspect element" on the square to see its effect.
-    return id === activeSquare ? 'active' : ""
-  };
+  //   if(id === activeSquare) {
+  //     return 'active'
+  //   } else {
+  //     return ''
+  //   }
+  // };
 
-  const markActive = id => {
+ 
     // This is a helper used inside an _inlined_ click handler (see below).
     // Set the id argument to become the active id in state
     // (unless it already is, in which case we should reset
     // the currently active square id back to initial state).
-    setavtiveSquare(id != activeSquare ? id : "")
-  };
+    const markActive = (element) => {
+      if (activeSquare === element) {
+        setActiveSquare('')
+      } else {
+        setActiveSquare(element)
+      }
+    };
 
   return (
     <div className='widget-squares container'>
@@ -50,12 +66,12 @@ const [activeSquare, setavtiveSquare] = useState("");
           // Nasty bug! We should map over a slice of state, instead of 'listOfSquareIds'.
           // We might say: "it works, though!" But if the list of squares is not state,
           // we could never add squares, change squares or remove squares in the future. Fix!
-          squares.map(id =>
+          squares.map(element =>
             <div
-              id={id}
-              key={id}
-              className={`square${isActive(id)}`}
-              onClick={() => markActive(id)}
+              id={element}
+              key={element}
+              className={`square ${getClassName(element)}`}
+              onClick={() => markActive(element)}
             >
             </div>
           )
